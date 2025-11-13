7905a1652ad9f01a911889b046accc696b8486bc
nate <natey3k@gmail.com>
2025-11-13 13:47:05 -0600

    monitorrules

--- config.def.h
+++ config.def.h
@@ -364,17 +364,9 @@ static const Rule rules[] = {
 
 #if MONITOR_RULES_PATCH
 #if PERTAG_PATCH
-static const MonitorRule monrules[] = {
-	/* monitor  tag   layout  mfact  nmaster  showbar  topbar */
-	{  1,       -1,   2,      -1,    -1,      -1,      -1     }, // use a different layout for the second monitor
-	{  -1,      -1,   0,      -1,    -1,      -1,      -1     }, // default
-};
+MONITOR_RULES_PERTAG_ARRAY
 #else
-static const MonitorRule monrules[] = {
-	/* monitor  layout  mfact  nmaster  showbar  topbar */
-	{  1,       2,      -1,    -1,      -1,      -1     }, // use a different layout for the second monitor
-	{  -1,      0,      -1,    -1,      -1,      -1     }, // default
-};
+MONITOR_RULES_DEFAULT_ARRAY
 #endif // PERTAG_PATCH
 #endif // MONITOR_RULES_PATCH
 
