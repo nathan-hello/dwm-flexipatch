749a3207cef4c3465e3585253179f254c3b19112
nate <natey3k@gmail.com>
2025-11-13 13:33:55 -0600

    scratchpads

--- config.def.h
+++ config.def.h
@@ -188,15 +188,7 @@ BAR_LTSYMBOL_SCHEME_CONSTS
 #endif // BAR_LTSYMBOL_SCHEME_PATCH
 
 #if RENAMED_SCRATCHPADS_PATCH
-static char scratchselfgcolor[]          = "#FFF7D4";
-static char scratchselbgcolor[]          = "#77547E";
-static char scratchselbordercolor[]      = "#894B9F";
-static char scratchselfloatcolor[]       = "#894B9F";
-
-static char scratchnormfgcolor[]         = "#FFF7D4";
-static char scratchnormbgcolor[]         = "#664C67";
-static char scratchnormbordercolor[]     = "#77547E";
-static char scratchnormfloatcolor[]      = "#77547E";
+RENAMED_SCRATCHPADS_CONSTS
 #endif // RENAMED_SCRATCHPADS_PATCH
 
 #if BAR_FLEXWINTITLE_PATCH
---
+++ patch/configs/scratchpads.h
@@ -0,0 +1,9 @@
+#define RENAMED_SCRATCHPADS_CONSTS \
+	static char scratchselfgcolor[]          = "#FFF7D4"; \
+	static char scratchselbgcolor[]          = "#77547E"; \
+	static char scratchselbordercolor[]      = "#894B9F"; \
+	static char scratchselfloatcolor[]       = "#894B9F"; \
+	static char scratchnormfgcolor[]         = "#FFF7D4"; \
+	static char scratchnormbgcolor[]         = "#664C67"; \
+	static char scratchnormbordercolor[]     = "#77547E"; \
+	static char scratchnormfloatcolor[]      = "#77547E";
