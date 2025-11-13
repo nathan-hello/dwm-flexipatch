e987a876318cf31c8d2a3f4d4e03886e54a4c548
nate <natey3k@gmail.com>
2025-11-13 13:28:36 -0600

    alttab

--- config.def.h
+++ config.def.h
@@ -108,13 +108,7 @@ BAR_UNDERLINETAGS_CONSTS
 #endif // NAMETAG_PATCH
 
 #if ALT_TAB_PATCH
-/* alt-tab configuration */
-static const unsigned int tabmodkey        = 0x40; /* (Alt) when this key is held down the alt-tab functionality stays active. Must be the same modifier as used to run alttabstart */
-static const unsigned int tabcyclekey      = 0x17; /* (Tab) when this key is hit the menu moves one position forward in client stack. Must be the same key as used to run alttabstart */
-static const unsigned int tabposy          = 1;    /* tab position on Y axis, 0 = top, 1 = center, 2 = bottom */
-static const unsigned int tabposx          = 1;    /* tab position on X axis, 0 = left, 1 = center, 2 = right */
-static const unsigned int maxwtab          = 600;  /* tab menu width */
-static const unsigned int maxhtab          = 200;  /* tab menu height */
+ALT_TAB_CONSTS
 #endif // ALT_TAB_PATCH
 
 /* Indicators: see patch/bar_indicators.h for options */
