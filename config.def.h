b9fb9cf44ec63db306ca8d0fc5058ad672a3b360
nate <natey3k@gmail.com>
2025-11-13 13:42:01 -0600

    powerline

--- config.def.h
+++ config.def.h
@@ -260,18 +260,7 @@ static char *colors[][ColCount] = {
 };
 
 #if BAR_POWERLINE_STATUS_PATCH
-static char *statuscolors[][ColCount] = {
-	/*                       fg                bg                border                float */
-	[SchemeNorm]         = { normfgcolor,      normbgcolor,      normbordercolor,      normfloatcolor },
-	[SchemeSel]          = { selfgcolor,       selbgcolor,       selbordercolor,       selfloatcolor },
-	[SchemeTitleNorm]    = { titlenormfgcolor, titlenormbgcolor, titlenormbordercolor, titlenormfloatcolor },
-	[SchemeTitleSel]     = { titleselfgcolor,  titleselbgcolor,  titleselbordercolor,  titleselfloatcolor },
-	[SchemeTagsNorm]     = { tagsnormfgcolor,  tagsnormbgcolor,  tagsnormbordercolor,  tagsnormfloatcolor },
-	[SchemeTagsSel]      = { tagsselfgcolor,   tagsselbgcolor,   tagsselbordercolor,   tagsselfloatcolor },
-	[SchemeHidNorm]      = { hidnormfgcolor,   hidnormbgcolor,   c000000,              c000000 },
-	[SchemeHidSel]       = { hidselfgcolor,    hidselbgcolor,    c000000,              c000000 },
-	[SchemeUrg]          = { urgfgcolor,       urgbgcolor,       urgbordercolor,       urgfloatcolor },
-};
+BAR_POWERLINE_STATUS_ARRAY
 #endif // BAR_POWERLINE_STATUS_PATCH
 
 #if BAR_LAYOUTMENU_PATCH
---
+++ patch/configs/powerline.h
@@ -0,0 +1,13 @@
+#define BAR_POWERLINE_STATUS_ARRAY \
+	static char *statuscolors[][ColCount] = { \
+		/*                       fg                bg                border                float */ \
+		[SchemeNorm]         = { normfgcolor,      normbgcolor,      normbordercolor,      normfloatcolor }, \
+		[SchemeSel]          = { selfgcolor,       selbgcolor,       selbordercolor,       selfloatcolor }, \
+		[SchemeTitleNorm]    = { titlenormfgcolor, titlenormbgcolor, titlenormbordercolor, titlenormfloatcolor }, \
+		[SchemeTitleSel]     = { titleselfgcolor,  titleselbgcolor,  titleselbordercolor,  titleselfloatcolor }, \
+		[SchemeTagsNorm]     = { tagsnormfgcolor,  tagsnormbgcolor,  tagsnormbordercolor,  tagsnormfloatcolor }, \
+		[SchemeTagsSel]      = { tagsselfgcolor,   tagsselbgcolor,   tagsselbordercolor,   tagsselfloatcolor }, \
+		[SchemeHidNorm]      = { hidnormfgcolor,   hidnormbgcolor,   c000000,              c000000 }, \
+		[SchemeHidSel]       = { hidselfgcolor,    hidselbgcolor,    c000000,              c000000 }, \
+		[SchemeUrg]          = { urgfgcolor,       urgbgcolor,       urgbordercolor,       urgfloatcolor }, \
+	};
