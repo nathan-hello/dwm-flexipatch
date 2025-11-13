f58672217c5b45891e50ac42213eca6da9abda5e
nate <natey3k@gmail.com>
2025-11-13 13:28:18 -0600

    systray

--- config.def.h
+++ config.def.h
@@ -82,8 +82,7 @@ BAR_STATUSPADDING_CONSTS
 BAR_STATUSBUTTON_CONSTS
 #endif // BAR_STATUSBUTTON_PATCH
 #if BAR_SYSTRAY_PATCH
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int showsystray             = 1;   /* 0 means no systray */
+BAR_SYSTRAY_CONSTS
 #endif // BAR_SYSTRAY_PATCH
 #if BAR_TAGLABELS_PATCH
 static const char ptagf[] = "[%s %s]";          /* format of a tag label */
