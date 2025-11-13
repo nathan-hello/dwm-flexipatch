82a6796cf1f04f0ba5bf00804369a3b7aaabde45
nate <natey3k@gmail.com>
2025-11-13 13:27:13 -0600

    vanitygaps

--- config.def.h
+++ config.def.h
@@ -24,11 +24,7 @@ BAR_TAGPREVIEW_CONSTS
 NO_MOD_BUTTONS_CONSTS
 #endif // NO_MOD_BUTTONS_PATCH
 #if VANITYGAPS_PATCH
-static const unsigned int gappih         = 20;  /* horiz inner gap between windows */
-static const unsigned int gappiv         = 10;  /* vert inner gap between windows */
-static const unsigned int gappoh         = 10;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = 30;  /* vert outer gap between windows and screen edge */
-static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
+VANITYGAPS_CONSTS
 #endif // VANITYGAPS_PATCH
 #if AUTOSTART_PATCH
 static const char autostartblocksh[]     = "autostart_blocking.sh";
---
+++ patch/configs/vanitygaps.h
@@ -0,0 +1,6 @@
+#define VANITYGAPS_CONSTS \
+	static const unsigned int gappih         = 20;  /* horiz inner gap between windows */ \
+	static const unsigned int gappiv         = 10;  /* vert inner gap between windows */ \
+	static const unsigned int gappoh         = 10;  /* horiz outer gap between windows and screen edge */ \
+	static const unsigned int gappov         = 30;  /* vert outer gap between windows and screen edge */ \
+	static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
