bdbceeef75775cf223cb12c082c26ee11b825ef8
nate <natey3k@gmail.com>
2025-11-13 16:36:18 -0600

    keymodes

--- patch/configs/keymodes.h
+++ patch/configs/keymodes.h
@@ -1,2 +1,28 @@
 #define KEYMODES_ENTRY \
 	{ MODKEY,                       XK_Escape,     setkeymode,             {.ui = COMMANDMODE} },
+
+#define KEYMODES_CMDKEYS \
+  static const Key cmdkeys[] = { \
+	/* modifier                    keys                     function         argument */ \
+	{ 0,                           XK_Escape,               clearcmd,        {0} }, \
+	{ ControlMask,                 XK_c,                    clearcmd,        {0} }, \
+	{ 0,                           XK_i,                    setkeymode,      {.ui = INSERTMODE} },\
+  };
+
+#define KEYMODES_COMMANDS \
+static const Command commands[] = { \
+    /* modifier (4 keys)                          keysyms (4 keys)                                function         argument */ \
+    { {ControlMask, ShiftMask,  0,         0},    {XK_w,      XK_h,     0,         0},            setlayout,       {.v = &layouts[0]} }, \
+    { {ControlMask, 0,          0,         0},    {XK_w,      XK_o,     0,         0},            setlayout,       {.v = &layouts[2]} }, \
+    { {ControlMask, ShiftMask,  0,         0},    {XK_w,      XK_o,     0,         0},            onlyclient,      {0} }, \
+    { {ControlMask, 0,          0,         0},    {XK_w,      XK_v,     0,         0},            setlayout,       {.v = &layouts[0]} }, \
+    { {ControlMask, 0,          0,         0},    {XK_w,      XK_less,  0,         0},            setmfact,        {.f = -0.05} }, \
+    { {ControlMask, ShiftMask,  0,         0},    {XK_w,      XK_less,  0,         0},            setmfact,        {.f = +0.05} }, \
+    { {ControlMask, ShiftMask,  0,         0},    {XK_w,      XK_0,     0,         0},            setmfact,        {.f = +1.50} }, \
+    { {ShiftMask,   0,          0,         0},    {XK_period, XK_e,     0,         0},            spawn,           {.v = dmenucmd} }, \
+    { {ShiftMask,   0,          0,         0},    {XK_period, XK_o,     0,         0},            spawn,           {.v = dmenucmd} }, \
+    { {ShiftMask,   0,          0,         0},    {XK_period, XK_q,     XK_Return, 0},            quit,            {0} }, \
+    { {ShiftMask,   0,          0,         0},    {XK_period, XK_b,     XK_d,      XK_Return},    killclient,      {0} }, \
+    { {ShiftMask,   0,          0,         0},    {XK_period, XK_b,     XK_n,      XK_Return},    focusstack,      {.i = +1} }, \
+    { {ShiftMask,   0,          ShiftMask, 0},    {XK_period, XK_b,     XK_n,      XK_Return},    focusstack,      {.i = -1} }, \
+};
