ec578b9f81ffa9015c9a718081343426676e8ce4
nate <natey3k@gmail.com>
2025-11-13 13:27:32 -0600

    tabpatch

--- config.def.h
+++ config.def.h
@@ -43,8 +43,7 @@ static const int topbar                  = 1;   /* 0 means bottom bar */
 /*  monocle mode in the presence of several windows.                        */
 /*  Modes after showtab_nmodes are disabled.                                */
 enum showtab_modes { showtab_never, showtab_auto, showtab_nmodes, showtab_always};
-static const int showtab                 = showtab_auto;        /* Default tab bar show mode */
-static const int toptab                  = False;               /* False means bottom tab bar */
+TAB_PATCH_CONSTS
 #endif // TAB_PATCH
 #if BAR_HEIGHT_PATCH
 static const int bar_height              = 0;   /* 0 means derive from font, >= 1 explicit height */
---
+++ patch/configs/tabpatch.h
@@ -0,0 +1,3 @@
+#define TAB_PATCH_CONSTS \
+	static const int showtab                 = showtab_auto;        /* Default tab bar show mode */ \
+	static const int toptab                  = False;               /* False means bottom tab bar */
